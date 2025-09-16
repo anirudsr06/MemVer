fd71183631b80e22790e3bfce4cc1a2dd3ddeb05
anirudsr06 <anirudsr06@gmail.com>
2025-09-16 19:26:26 +0530

    added functions

--- trees/mkt-sha256.cpp
+++ trees/mkt-sha256.cpp
@@ -30,15 +30,17 @@ struct bNode{
   bNode(const string& to_hash) : hash(to_hash), parent(nullptr), sibling(nullptr) {}
 };
 
-bNode* buildMerkleTree(vector<string> data_block){
+bNode* buildMerkleTree(vector<string> data_block, vector<bNode*>& leaves){
   vector<bNode*> currentLevel;
 
   for (const auto block : data_block ){
-    currentLevel.push_back(new bNode(sha256(block)));
+    bNode* leaf = new bNode(sha256(block));
+    currentLevel.push_back(leaf);
+    leaves.push_back(leaf);
   }
 
   if (currentLevel.size()%2 == 1){
-    currentLevel.push_back(new bNode(currentLevel.back() -> hash));
+    currentLevel.push_back(new bNode(sha256("0")));
   }
 
   while (currentLevel.size() > 1) {
@@ -60,7 +62,7 @@ bNode* buildMerkleTree(vector<string> data_block){
     }
 
     if (nextLevel.size()% 2 == 1 && nextLevel.size() > 1){
-      nextLevel.push_back(new bNode(nextLevel.back() -> hash));
+      nextLevel.push_back(new bNode(sha256("0")));
     }
 
     currentLevel = move(nextLevel);
@@ -69,13 +71,27 @@ bNode* buildMerkleTree(vector<string> data_block){
   return currentLevel.front();
 }
 
+void updateMerkleTree(bNode* leaf, string new_val){
+  leaf -> hash = sha256(new_val);
+  bNode* current = leaf;
+  while (current -> parent != nullptr) {
+    bNode* next = current -> parent;
+    bNode* sibling = current -> sibling;
+    next -> hash = sha256(current -> hash + sibling -> hash);
+    current = next;
+  }
+}
 
 int main (){
-  vector<string> data_block = { "block", "block2", "block3", "block4"};
+  vector<string> data_block = { "block1", "block2", "block3", "block4", "block5"};
+  vector<bNode*> leaves;
+  bNode* root = buildMerkleTree(data_block, leaves);
+  
+  cout << "Original Merkle Root: " << root-> hash << endl;
+  updateMerkleTree(leaves[4], "block_2");
 
-  bNode* root = buildMerkleTree(data_block);
+  cout << "Updated Merkle Root: " << root-> hash << endl;
 
-  cout << "Merkle Root: " << root-> hash << endl;
   return 0;
 }
 
